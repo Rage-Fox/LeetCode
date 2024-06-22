8e77b7dd4f8d16c99241773ce0cdb7705523efae
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2024-06-22 05:41:25 +0530

    Time: 168 ms (13.21%), Space: 93.8 MB (17.87%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int subArray(vector<int> &nums,int k){
-        int count=0,ans=0,start=0,end=0,n=nums.size();
-        while(end<n){
-            if(nums[end]%2==1){
+    int numberOfSubarrays(vector<int> &nums,int k){
+        unordered_map<int,int>m;
+        int ans=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2==1){
                 count++;
             }
-            while(count>k){
-                if(nums[start]%2==1){
-                    count--;
-                }
-                start++;
+            if(count==k){
+                ans++;
             }
-            ans+=end-start+1;
-            end++;
+            int n=count-k;
+            if(m.find(n)!=m.end()){
+                ans=ans+m[n];
+            }
+            m[count]++;
+
         }
         return ans;
-    }
-    int numberOfSubarrays(vector<int> &nums,int k){
-        // Exactly k = Less than or equal to (k) - Less than or equal to (k-1)
-        return subArray(nums,k)-subArray(nums,k-1);
     }
 };
