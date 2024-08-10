d45e7026bee7beab8898e8fde9f1156922965619
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2024-08-10 07:36:05 +0530

    Time: 13 ms (46.63%), Space: 10.5 MB (88.82%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string convert(string s, int n) {
+        if(n==1)
+            return s;
+        string ans="";
+        for(int row=0;row<n;row++){
+            // for each row the next chracter is at index 2*(n-1) 
+            int increment=2*(n-1);
+            // For first and last rows
+            for(int i=row;i<s.length();i+=increment){
+                ans+=s[i];
+                // For middle rows there will be extra characters
+                if(row>0 && row<n-1 && i+increment-2*row<s.length()){
+                    ans+=s[i+increment-2*row];
+                }
+            }
+        }
+        return ans;
+    }
+};
