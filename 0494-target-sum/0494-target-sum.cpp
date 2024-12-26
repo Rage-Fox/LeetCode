e0fbb5cd69006c1189871b2c7e510615ab10290d
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2024-12-26 07:25:23 +0530

    Time: 19 ms (54.77%), Space: 41.1 MB (21.56%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int func(vector<vector<int>>& dp,vector<int>& v,int num,int ind){
+        int n=v.size();
+        if(ind==n){
+            return num==0;
+        }
+        if(num+1000<0 or num+1000>=2001){
+            return 0;
+        }
+        if(dp[ind][num+1000]!=-1){
+            return dp[ind][num+1000];
+        }
+        int ntake=func(dp,v,num+v[ind],ind+1);
+        int take=func(dp,v,num-v[ind],ind+1);
+        return dp[ind][num+1000]=(take+ntake);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>> dp(n, vector<int>(2001,-1));
+        return func(dp,nums,target,0);
+    }
+};
