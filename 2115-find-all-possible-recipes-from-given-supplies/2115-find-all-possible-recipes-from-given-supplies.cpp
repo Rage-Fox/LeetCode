80a7b2d785ae80883e1950d38bdc58a3c4affe64
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-03-21 06:00:33 +0530

    Time: 132 ms (95.02%), Space: 165 MB (32.16%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+        unordered_map<string,vector<string>> adj;
+        unordered_map<string,int> indegree;
+        for(int i=0;i<ingredients.size();i++){
+            for(auto j:ingredients[i]){
+                adj[j].push_back(recipes[i]); 
+                indegree[recipes[i]]++;
+            }
+        }
+        queue<string> q;
+        for(auto i:supplies){
+            q.push(i);
+        }
+        vector<string> ans;
+        while(!q.empty()){
+            string node=q.front();
+            q.pop();
+            for(auto i:adj[node]){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    q.push(i);
+                    ans.push_back(i);
+                }
+            }
+        }
+        return ans;
+    }
+};
