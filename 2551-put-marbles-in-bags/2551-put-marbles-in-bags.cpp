e1f8ab19cf56f8b0dd81f8ad8d26d7830bda86d8
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-03-31 08:11:56 +0530

    Time: 31 ms (89.03%), Space: 67.6 MB (44.3%) - LeetHub

---
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        // Hints: https://leetcode.com/problems/put-marbles-in-bags/solutions/6598048/hints-that-will-guide-you-to-the-solution-yourself
+        if(k==1){
+            return 0;
+        }
+        vector<int> pair_sums;
+        for(int i=0;i<weights.size()-1;i++){
+            pair_sums.push_back(weights[i]+weights[i+1]);
+        }
+        sort(pair_sums.begin(),pair_sums.end());
+        long long min_score=accumulate(pair_sums.begin(),pair_sums.begin()+(k-1),0LL);
+        long long max_score=accumulate(pair_sums.end()-(k-1),pair_sums.end(),0LL);
+        return max_score-min_score;
+    }
+};
