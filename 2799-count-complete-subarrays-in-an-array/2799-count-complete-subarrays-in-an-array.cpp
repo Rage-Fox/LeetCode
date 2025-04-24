0d12f591225f9f34a61bf320cd0e3cd9d839adea
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-04-24 06:21:35 +0530

    Time: 23 ms (51.27%), Space: 41.7 MB (71.22%) - LeetHub

---
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countCompleteSubarrays(vector<int>& nums) {
+        int res=0,n=nums.size(),right=0;
+        unordered_map<int,int> cnt;
+        unordered_set<int> distinct(nums.begin(),nums.end());
+        int distinct_count=distinct.size();
+        for(int left=0;left<n;left++){
+            if(left>0){
+                int remove=nums[left-1];
+                cnt[remove]--;
+                if(cnt[remove]==0){
+                    cnt.erase(remove);
+                }
+            }
+            while(right<n && cnt.size()<distinct_count){
+                int add=nums[right];
+                cnt[add]++;
+                right++;
+            }
+            if(cnt.size()==distinct_count){
+                res+=(n-right+1);
+            }
+        }
+        return res;
+    }
+};
