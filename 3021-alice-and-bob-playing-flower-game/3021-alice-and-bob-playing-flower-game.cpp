f24a7918419a823042cd55c4de3f30fce6877c4e
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-08-29 06:38:52 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.89%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        // https://leetcode.com/problems/alice-and-bob-playing-flower-game/solutions/7133028/one-line-solution-most-efficient-beat-100-java-python-c-go-javascript-rust
+        return (long long)m*n/2;
+    }
+};
