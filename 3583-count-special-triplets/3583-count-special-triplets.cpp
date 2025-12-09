a4ce8bd4b898e37e73e0aa39d623df8ee66f1020
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-12-09 06:42:44 +0530

    Time: 854 ms (57.3%), Space: 427.8 MB (44.52%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        const int mod=1e9+7;
+        unordered_map<int,int> suffix;
+        unordered_map<int,int> prefix;
+        for(auto x:nums){
+            suffix[x]++;
+        }
+        long long ans=0;
+        for(auto x:nums){
+            suffix[x]--;
+            ans+=(long long)prefix[2*x]*suffix[2*x];
+            prefix[x]++;
+        }
+        return ans%mod;
+    }
+};
