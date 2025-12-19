2d1f3417b16ef3ebd8cf24427dac7ffe4540e43f
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2025-12-19 06:30:53 +0530

    Time: 0 ms (100%), Space: 14.3 MB (30.95%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -4,8 +4,9 @@ public:
         vector<int> result=prices;
         stack<int> stack;// To store indexes
         for(int i=0;i<prices.size();i++){
-            // If current index's element is less than stack's top index element, then current index element will be the discount
-            while(!stack.empty() and prices[stack.top()]>=prices[i]){
+            // If current index's element is less than stack's top index element,
+            // then current index element will be the discount
+            while(!stack.empty() && prices[stack.top()]>=prices[i]){
                 result[stack.top()]-=prices[i];
                 stack.pop();
             }
