5c857cd0a911ef5d7fcb76a0b134cbef1c19b11a
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-04 08:37:01 +0530

    Time: 0 ms (100%), Space: 10.6 MB (79.18%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        // Any number i can be written as: i = (highestPowerOfTwo ≤ i) + remainder
+        vector<int> dp(n+1);
+        int offset=1;
+        for(int i= 1;i<=n;i++){
+            if(offset*2==i){
+                offset=i;
+            }
+            dp[i]=1+dp[i-offset];
+        }
+        return dp;
+    }
+};
