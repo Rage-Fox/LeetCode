f3b150c7122af47f038ee53aa2c369e586587226
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-06 07:24:24 +0530

    Time: 12 ms (57.25%), Space: 56.8 MB (38.33%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -4,16 +4,15 @@ public:
         int n=box.size(),m=box[0].size();
         vector<vector<char>> new_box(m,vector<char>(n,'.'));
         for(int row=0;row<n;row++){
+            int lowest_empty_cell=m-1;
             for(int col=m-1;col>=0;col--){
                 if(box[row][col]=='*'){
                     new_box[col][n-1-row]='*';
+                    lowest_empty_cell=col-1;
                 }
                 else if(box[row][col]=='#'){
-                    int empty_col=col;
-                    while(empty_col+1<m and box[row][empty_col+1]=='.'){
-                        empty_col++;
-                    }
-                    swap(box[row][col],box[row][empty_col]);
+                    swap(box[row][lowest_empty_cell],box[row][col]);
+                    lowest_empty_cell--;
                 }
             }
         }
