5f6b835845642ca7863f6e46d8df2d2996437054
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-11 13:19:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start=0,end=nums.size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(target==nums[mid]){
-                return mid;
-            } 
-            else if(target<nums[mid]){
-                end=mid-1;
-            }
-            else if(target>nums[mid]){
-                start=mid+1;
-            }
-        }
-        return -1;
+        auto it=lower_bound(nums.begin(),nums.end(),target);
+        return (it!=nums.end() && *it==target)?(it-nums.begin()):-1;
     }
 };
