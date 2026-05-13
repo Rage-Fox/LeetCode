20cc2519d1f6c56baf632505ed7dce4e6c5b4132
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-13 11:28:59 +0530

    Time: 3 ms (12.95%), Space: 11.2 MB (59.09%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumPopulation(vector<vector<int>>& logs) {
+        map<int,int> line;
+        for(auto &p:logs){
+            line[p[0]]++;
+            line[p[1]]--;
+        }
+        int max_p=0,ans_year,count=0;
+        for(auto &i:line){
+            count+=i.second;
+            if(count>max_p){
+                max_p=count;
+                ans_year=i.first;
+            }
+        }
+        return ans_year;
+    }
+};
