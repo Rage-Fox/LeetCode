734d25d298e33265894ae42240ce5f3b47167ad0
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-13 11:23:08 +0530

    Time: 15 ms (43.36%), Space: 93.3 MB (83.19%) - LeetHub

---
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int limit) {
+        int n=nums.size();
+        vector<int> diff(2*limit+2,0);
+        for(int i=0;i<n/2;i++){
+            int mini=min(nums[i],nums[n-1-i]);
+            int maxi=max(nums[i],nums[n-1-i]);
+            int sum=mini+maxi;
+            diff[2]+=2;
+            diff[2*limit+1]-=2;
+            diff[mini+1]--;
+            diff[maxi+limit+1]++;
+            diff[sum]--;
+            diff[sum+1]++;
+        }
+        int ans=INT_MAX,moves=0;
+        for(int i=2;i<=2*limit;i++){
+            moves+=diff[i];
+            ans=min(ans,moves);
+        }
+        return ans;
+    }
+};
