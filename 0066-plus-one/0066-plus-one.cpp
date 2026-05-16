aa5825a7569904c6ed1d5e4acc0ffe1e617d88ec
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-16 15:28:17 +0530

    Time: 0 ms (100%), Space: 11.6 MB (61.63%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for(int i=digits.size()-1;i>=0;i--){
-            if(digits[i]+1!=10){
-                digits[i]+=1;
+        int n=digits.size();
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]++;
                 return digits;
             }
             digits[i]=0;
-            if(i==0){
-                digits.insert(digits.begin(),1);
-                return digits;
-            }
         }
-        return digits;
+        // after this loop, the entire number just became from 9999 to 0000, we just need to add 1 infront
+        vector<int> result(n+1,0);
+        result[0]=1;
+        return result;
     }
 };
