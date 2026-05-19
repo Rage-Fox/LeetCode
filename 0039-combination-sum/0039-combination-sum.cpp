fb8365daef91307170ddc7f581649ee95d79b2b5
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-19 21:49:37 +0530

    Time: 91 ms (6.05%), Space: 75.8 MB (5.67%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> ds;
+    vector<vector<int>> ans;
+    void f(int i,vector<int> nums,vector<int> ds,int target){
+        if(i==nums.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+        // Pick the current element multiple times
+        if(target>=nums[i]){
+            ds.push_back(nums[i]);
+            f(i,nums,ds,target-nums[i]);
+            ds.pop_back();
+        }
+        // Not picking the current element and moving to next element
+        f(i+1,nums,ds,target);
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        f(0,nums,ds,target);
+        return ans;
+    }
+};
