b29e09d14afe57ae0eaf12b4877dad0881252614
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-22 15:56:06 +0530

    Time: 0 ms (100%), Space: 8 MB (52.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int one = 1, two = 1;
-        for (int i = 0; i < n - 1; i++) {
-            int temp = one;
-            one = one + two;
-            two = temp;
-        }
-        return one;
+        // Binet's Formula - Calculating Fib(n+1) without it's previous values
+        double sqrt5=sqrt(5);
+        double phi=(1+sqrt5)/2;
+        double psi=(1-sqrt5)/2;
+        n++;
+        return round((pow(phi,n)-pow(psi,n))/sqrt5);
     }
 };
