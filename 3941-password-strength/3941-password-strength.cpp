1daeb49f5d42d5666d9b4e6153d54ed692c3c48c
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-24 10:37:58 +0530

    Time: 23 ms (39.7%), Space: 17.8 MB (24.26%) - LeetHub

---
+++ 3941-password-strength/3941-password-strength.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool small(char c) { return c <= 'z' and c >= 'a'; }
+    bool big(char c) { return c <= 'Z' and c >= 'A'; }
+    bool num(char c) { return c <= '9' and c >= '0'; }
+    bool sp(char c) { return c == '!' or c == '@' or c == '#' or c == '$'; }
+    int passwordStrength(string password) {
+        int ans = 0;
+        unordered_set<char> st(password.begin(), password.end());
+        for (auto c : st) {
+            if (small(c))
+                ans++;
+            else if (big(c))
+                ans += 2;
+            else if (num(c))
+                ans += 3;
+            else if (sp(c))
+                ans += 5;
+        }
+        return ans;
+    }
+};
