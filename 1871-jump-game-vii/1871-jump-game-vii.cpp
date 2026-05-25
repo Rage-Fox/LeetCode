a5efc5ba0865325b3ee3ffd41af04a879df2e007
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-05-25 07:19:29 +0530

    Time: 21 ms (13.6%), Space: 33.6 MB (16.18%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        queue<int> q;
+        int n = s.size();
+        vector<int> vis(n + 1, 0);
+        q.push(0);
+        int mxreach = 0;
+        while (!q.empty()) {
+            int idx = q.front();
+            q.pop();
+            if (idx == n - 1) {
+                return true;
+            }
+            for (int i = (int)(max(mxreach, idx + minJump)); i <= (int)(min(idx + maxJump, n - 1)); i++) {
+                if (vis[i] == 0 && s[i] == '0') {
+                    q.push(i);
+                    vis[i] = 1;
+                }
+            }
+            // used in loop to check current index+minJump is better or else prevjump(maxJump)+1 is better
+            mxreach = max(mxreach, idx + maxJump + 1);
+        }
+        return false;
+    }
+};
