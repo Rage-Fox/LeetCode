ebc3b85aa1b1679dabdd053e20531145e7b5be13
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-06-14 18:48:16 +0530

    Time: 3 ms (33.48%), Space: 13.2 MB (71.26%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
     vector<int> parent;
-    int findUltimateParent(int x){
-        if(parent[x]==x){
-            return x;
+    int findUltimateParent(int x) {
+        int p = parent[x];
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
         }
-        return parent[x]=findUltimateParent(parent[x]);
+        return p;
     }
-    void unionGraph(int x,int y){
-        int a=findUltimateParent(x);
-        int b=findUltimateParent(y);
-        parent[b]=parent[a];
+    void unionGraph(int x, int y) {
+        int a = findUltimateParent(x);
+        int b = findUltimateParent(y);
+        parent[b] = parent[a];
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        parent.resize(edges.size()+1);
-        int ind=-1;
-        for(int i=0;i<=edges.size();i++){
-            parent[i]=i;
+        parent.resize(edges.size() + 1);
+        int ind = -1;
+        for (int i = 0; i <= edges.size(); i++) {
+            parent[i] = i;
         }
-        for(int i=0;i<edges.size();i++){
-            if(findUltimateParent(edges[i][0])!=findUltimateParent(edges[i][1]))
-                unionGraph(edges[i][0],edges[i][1]);
-            else{
-                ind=i;
+        for (int i = 0; i < edges.size(); i++) {
+            if (findUltimateParent(edges[i][0]) != findUltimateParent(edges[i][1])) {
+                // connect and form a graph if ultimate parents are not same
+                unionGraph(edges[i][0], edges[i][1]);
+            }
+            else {
+                ind = i;
             }
         }
         return edges[ind];
