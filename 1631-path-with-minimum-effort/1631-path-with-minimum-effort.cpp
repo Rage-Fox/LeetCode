566cce4ede3e09394aa5da8004fedb04aec38499
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-06-18 17:52:35 +0530

    Time: 53 ms (61.4%), Space: 24.3 MB (96.95%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq; // Min Heap
+        int n = heights.size();
+        int m = heights[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        dist[0][0] = 0;
+        pq.push({0, {0, 0}});
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+        // Dijkstra's Algorithm
+        while (!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+            int diff = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            if (row == n - 1 && col == m - 1)
+                return diff;
+            for (int i = 0; i < 4; i++) {
+                int newr = row + dr[i];
+                int newc = col + dc[i];
+                if (newr >= 0 && newc >= 0 && newr < n && newc < m) {
+                    // Calculate the effort required to move to the new cell
+                    int newEffort =
+                        max(abs(heights[row][col] - heights[newr][newc]), diff);
+                    if (newEffort < dist[newr][newc]) {
+                        dist[newr][newc] = newEffort;
+                        pq.push({newEffort, {newr, newc}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
