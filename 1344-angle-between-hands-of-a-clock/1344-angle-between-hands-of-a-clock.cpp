9a5ff257516ae3d882ff525683548e91ca86b906
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-06-18 06:46:55 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.25%) - LeetHub

---
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        // https://leetcode.com/problems/angle-between-hands-of-a-clock/solutions/8340833/0ms-beats-10000-easy-approach-and-step-b-bied
+        // angle in 1 minute = 360/60 = 6 degree
+        // angle in 1 hour = 360/12 = 30 degree
+        hour %= 12;
+        double minuteAngle = minutes * 6.0;
+        double hourAngle = hour * 30.0 + minutes * 0.5;
+        double diff = abs(hourAngle - minuteAngle);
+        return min(diff, 360.0 - diff);
+    }
+};
