ffcd30d1b989935603a3852a16187f429c7455cf
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-06-29 07:46:48 +0530

    Time: 153 ms (20.86%), Space: 214.1 MB (15.89%) - LeetHub

---
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& occupiedIntervals, int freeStart, int freeEnd) {
+        sort(occupiedIntervals.begin(), occupiedIntervals.end());
+        vector<vector<int>> temp;
+        for (auto it : occupiedIntervals) {
+            if (temp.empty() || temp.back()[1] + 1 < it[0]) {
+                temp.push_back(it);
+            } else {
+                temp.back()[1] = max(temp.back()[1], it[1]);
+            }
+        }
+        vector<vector<int>> res;
+        for (auto it : temp) {
+            int s = it[0];
+            int e = it[1];
+            if (e < freeStart || s > freeEnd) {
+                res.push_back({s, e});
+            } else {
+                if (s < freeStart)
+                    res.push_back({s, freeStart - 1});
+                if (e > freeEnd)
+                    res.push_back({freeEnd + 1, e});
+            }
+        }
+        return res;
+    }
+};
