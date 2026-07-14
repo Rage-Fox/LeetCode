2ab660d99d2485a793a950e15250c34222db2b3b
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-07-14 13:01:57 +0530

    Time: 4 ms (19.11%), Space: 24.4 MB (49.48%) - LeetHub

---
+++ 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isprime(int n) {
+        if (n < 2){
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkPrimeFrequency(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for (int num : nums) {
+            mpp[num]++;
+        }
+        for (auto it : mpp) {
+            if (isprime(it.second)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
