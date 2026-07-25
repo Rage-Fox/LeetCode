31b1d14f26e71ba4b814b0e4ce2b256863bd2eab
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-07-25 12:08:38 +0530

    Time: 3 ms (26.44%), Space: 10.3 MB (25.44%) - LeetHub

---
+++ 3992-rearrange-string-to-avoid-character-pair/3992-rearrange-string-to-avoid-character-pair.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string rearrangeString(string s, char x, char y) {
+        string xc = "", yc = "", others = "";
+        for (char c : s) {
+            if (c == y)
+                yc += c;
+            else if (c == x)
+                xc += c;
+            else
+                others += c;
+        }
+        return yc + others + xc;
+    }
+};
