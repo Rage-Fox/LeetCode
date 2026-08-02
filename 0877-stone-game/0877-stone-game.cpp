1911854219ed2389608fd3f51ef85233b3a79356
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2026-08-02 07:58:44 +0530

    Time: 0 ms (100%), Space: 10.4 MB (87.75%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        int alice=0, bob=0,size=piles.size();
-        for(int i=0;i<size/2;i++){
-            alice=alice+max(piles[i],piles[size-1-i]);
-            bob=bob+min(piles[i],piles[size-1-i]);
+        int alice = 0, bob = 0, size = piles.size();
+        for (int i = 0; i < size / 2; i++) {
+            alice = alice + max(piles[i], piles[size - 1 - i]);
+            bob = bob + min(piles[i], piles[size - 1 - i]);
         }
-        if(alice>bob)  
+        if (alice > bob)
             return true;
         return false;
     }
